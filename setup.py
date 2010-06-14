585ccf22e8f2139bce7675d231582ea11a4d85ec
shaunsephton <shaunsephton@gmail.com>
2010-06-14 18:11:53 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-pagemenu',
-    version='dev',
+    version='0.0.1',
     description='Django app for page menus with items altering querysets or linking directly to other views.',
-    author='Praekelt Consulting',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-pagemenu',
+    license='BSD',
+    url='http://github.com/praekelt/django-pagemenu',
     packages = find_packages(),
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
